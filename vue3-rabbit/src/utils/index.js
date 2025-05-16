fe221b5b2d4b3acce75a78671c31f474df4f5be1
Exxxvvv <2127563914@qq.com>
2025-05-17 01:08:33 +0800

    axios的基础封装

---
+++ vue3-rabbit/src/utils/index.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+const httpInstance = axios.create({
+  baseURL: 'http://pcapi-xiaotuxian-front-devtest.itheima.net',
+  timeout: 5000
+})
+// axios请求拦截器
+instance.interceptors.request.use(config => {
+  return config
+})
+// axios响应式拦截器
+instance.interceptors.response.use(res => res.data, e=> {
+  return Promise.reject(e)
+})
+export default httpInstance
