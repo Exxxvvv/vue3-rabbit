d854ac149c6af2173363689de73114cc9e4f1e12
Exxxvvv <2127563914@qq.com>
2025-05-17 16:09:38 +0800

    字体图标的引入

--- vue3-rabbit/src/main.js
+++ vue3-rabbit/src/main.js
@@ -3,6 +3,7 @@ import { createPinia } from 'pinia'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 // 引入初始化的样式文件
+import '@/styles/common.scss'
 
 import App from './App.vue'
 import router from './router'
--- vue3-rabbit/src/views/Layout/index.vue
+++ vue3-rabbit/src/views/Layout/index.vue
@@ -1,12 +1,17 @@
 <template>
   <div class="layout">
-    layout
+    <LayoutNav></LayoutNav>
+    <LayoutHeader></LayoutHeader>
     <RouterView></RouterView>
+    <LayoutFooter></LayoutFooter>
   </div>
 </template>
 
 <script setup name="Layout">
   import {RouterView} from 'vue-router'
+  import LayoutNav from './components/LayoutNav.vue';
+  import LayoutHeader from './components/LayoutHeader.vue';
+  import LayoutFooter from './components/LayoutFooter.vue';
 </script>
 
 <style scoped>
