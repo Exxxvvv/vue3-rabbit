b6f2419c07331ab27a8f9b24ed08b20d339eba9d
Exxxvvv <2127563914@qq.com>
2025-05-18 01:14:27 +0800

    layout吸顶导航交互实现

--- vue3-rabbit/src/views/Layout/index.vue
+++ vue3-rabbit/src/views/Layout/index.vue
@@ -4,11 +4,13 @@
     <LayoutHeader></LayoutHeader>
     <RouterView></RouterView>
     <LayoutFooter></LayoutFooter>
+    <LayoutFixed></LayoutFixed>
   </div>
 </template>
 
 <script setup name="Layout">
   import {RouterView} from 'vue-router'
+  import LayoutFixed from './components/LayoutFixed.vue';
   import LayoutNav from './components/LayoutNav.vue';
   import LayoutHeader from './components/LayoutHeader.vue';
   import LayoutFooter from './components/LayoutFooter.vue';
